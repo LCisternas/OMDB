5ecf8ada7514f43fca55d4cc3b7fd5fdd7ad5c46
Lucas Cisternas <l.cisternasopazo@gmail.com>
2021-09-16 23:33:20 -0300

    views

--- client/src/components/account/Account.jsx
+++ client/src/components/account/Account.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Navbar from '../navbar/Navbar';
+import style from './account.module.css';
 
 const Account = () => {
   return (
-    <div>
-      <h1>Account</h1>
+    <div className={style.accountContainer}>
+      <div className={style.accountNavbar}>
+        <Navbar />
+      </div>
+      <div className={style.accountContent}>
+        <div className={style.accountContentTitle}>
+          <h1>Tus datos personales</h1>
+        </div>
+        <div className={style.accountContentActions}>
+          <h2>Nombre : <span>Lucas Cisternas</span></h2>
+          <h2>Email : <span>lucas@gmail.com</span></h2>
+          <button>Cambiar Email</button>
+          <button>Cambiar contraseña</button>
+          <button>Eliminar cuenta</button>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/components/favorites/Favorites.jsx
+++ client/src/components/favorites/Favorites.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Navbar from '../navbar/Navbar';
+import style from './favorites.module.css';
 
 const Favorites = () => {
   return (
-    <div>
-      <h1>Favorites</h1>
+    <div className={style.favoritesContainer}>
+      <div className={style.favoritesNavbar}>
+        <Navbar />
+      </div>
+      <div className={style.favoritesContent}>
+        <h1> Tus Peliculas favoritas</h1>
+      </div>
     </div>
   );
 }
