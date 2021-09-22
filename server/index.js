2ade9c791c2376ab3b2d4aa83f79a88bcc08947d
Lucas Cisternas <l.cisternasopazo@gmail.com>
2021-09-21 23:32:24 -0300

    iniciando servidor

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+app.listen(PORT, () => {
+  console.log('RUN SERVER')
+})
